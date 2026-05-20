0816d110033a1ead219c98cd5a3cff3265139925
Will Parsons-Douglas <will@parsons-douglas.me>
2026-05-20 09:52:22 +0100

    Fix switch constant warning

--- 100-DOS-Playground.playground/Contents.swift
+++ 100-DOS-Playground.playground/Contents.swift
@@ -448,8 +448,97 @@ print(daily2)
 
 print(daily3)
 
+//Day 5 - how to check multiple conditions
 
+/*
+ 
+ var age1 = 16
 
+ if age1 >= 18 {
+    print("You can vote in the next election")
+} else {
+    print("Sorry, you're too young to vote")
+}
+ 
+ if a {
+   print("Code to run if a is true")
+ } else if b {
+ 
+ print("Code to run if a is false but b is true")}
+ 
+ else {
+   print("Code to run if both a and b are false")}
+ 
+ */
 
+let temp = 25
+
+// both conditions must be true - &&
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+//OR operator - either the first condition or second must be true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter }
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print ("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there is a bike path")
+} else if transport == .car {
+    print("Time to get stuck in traffic")
+} else {
+    print("Time to scoot")
+}
+
+//switch statement
+
+enum Weather {
+    case sun, rain, wind, snow, unknown
+}
+
+let forecast = Weather.rain
+
+switch forecast {
+case .sun:
+    print("It should be a nice day")
+case .rain:
+    print("Pack an umbrella")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("School might be cancelled")
+case .unknown:
+    print("The generator is broken!")
+}
+
+// ternary conditional operator - need more readup - not exactly sure on this one
+
+let age2 = 18
+
+let canVote = age >= 18 ? "Yes" : "No"
+
+print (canVote)
+
+let hour = 23
+
+print(hour < 12 ? "It's before noon" : "It's after noon")
+
+let names3 = ["Jayne", "Kaylee", "Mal"]
+
+let crewCount = name.isEmpty ? "No one" : "\(names3.count) people"
+
+print (crewCount)
 
 
